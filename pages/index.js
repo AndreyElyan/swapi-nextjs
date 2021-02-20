6c57b4e2f998a8f55d8d6e55bd6cfe8f3b2194eb
Andrey Elyan <ilegra.andrey@agibank.com.br>
2021-02-19 23:58:51 -0300

    feat(tailwind): added tailwindo to project

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next Appsjbdjsbdjb</title>
+        <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+      <main className="bg-blue-200">
+        <h1>
+          Welcome to <a href="https://nextjs.org">Next.jsx!</a>
         </h1>
 
         <p className={styles.description}>
---
+++ tailwind.config.js
@@ -0,0 +1,95 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  important: true,
+  corePlugins: {
+    float: false,
+  },
+  theme: {
+    extend: {
+      spacing: {
+        14: "3.5rem",
+      },
+    },
+    fontFamily: {
+      averta: [
+        "Averta Std",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+        '"Noto Sans"',
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      icons: "agibank-icons",
+    },
+    fontSize: {
+      "3xs": "0.75rem", // 12px
+      "2xs": "0.875rem", // 14px
+      xs: "1rem", // 16px
+      sm: "1.125rem", // 18px
+      base: "1.25rem", // 20px
+      lg: "1.5rem", // 24px
+      xl: "1.75rem", // 28px
+      "2xl": "2rem", // 32px
+      "3xl": "2.5rem", // 40px
+      "4xl": "2.75rem", // 44px
+      "5xl": "3rem", // 48px
+      "6xl": "3.5rem", // 56px
+      "7xl": "4rem", // 64px
+      "8xl": "4.5rem", // 72px
+      "9xl": "6rem", // 96px
+      "10xl": "14.375rem", // 230px
+    },
+    lineHeight: {
+      none: "1",
+      tight: "1.2",
+      snug: "1.33",
+      normal: "1.5",
+      relaxed: "1.625",
+      loose: "2",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+    },
+    colors: {
+      transparent: "transparent",
+      black: "#000",
+      white: "#fff",
+      gray: {
+        100: "#f2f2f2",
+        200: "#d3d3d3",
+        300: "#c7cad9",
+        400: "#7a91ad",
+      },
+      red: "#ff001f",
+      orange: "#f5a623",
+      green: "#2fc750",
+      blue: {
+        100: "#266bff",
+        200: "#1e55cb",
+        300: "#262857",
+      },
+    },
+    willChange: {
+      // defaults to these values
+      auto: "auto",
+      scroll: "scroll-position",
+      contents: "contents",
+      opacity: "opacity",
+      transform: "transform",
+    },
+  },
+  variants: {},
+  plugins: [],
+};
