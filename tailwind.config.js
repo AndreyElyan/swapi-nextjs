cea725f9fb3810f1319476166ccb0cd362b68224
Andrey Elyan <ilegra.andrey@agibank.com.br>
2021-02-21 23:46:42 -0300

    fix(css): tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './containers/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
   important: true,
   corePlugins: {
     float: false
