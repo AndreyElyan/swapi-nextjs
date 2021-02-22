ee9a960282444f544dbbd16c56d79b155ce27670
Andrey Elyan <ilegra.andrey@agibank.com.br>
2021-02-21 23:07:53 -0300

    fix(css): tailwind fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,3 @@
-// postcss.config.js
-const purgecss = [
-  '@fullhuman/postcss-purgecss',
-  {
-    content: [
-      './node_modules/megadraft/dist/**/*.css',
-      './components/**/*.js',
-      './pages/**/*.js'
-    ],
-
-    defaultExtractor: content => {
-      const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
-      const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
-      return broadMatches.concat(innerMatches);
-    }
-  }
-];
-
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    'autoprefixer',
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
-  ]
+  plugins: ['tailwindcss', 'postcss-preset-env']
 };
