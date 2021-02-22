194aa12ddc4932a7a059e7ee4ee4cd993b9606ca
Andrey Elyan <ilegra.andrey@agibank.com.br>
2021-02-21 22:58:19 -0300

    fix(css): fix css prod

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,3 @@
-// tailwind.config.css
-module.exports = {
-  purge: ['./components/**/*.js', './pages/**/*.js'],
-  theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: []
-};
-
 module.exports = {
   plugins: ['tailwindcss', 'postcss-preset-env']
 };
