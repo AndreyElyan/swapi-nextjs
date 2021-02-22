0d7fdc3143b03c104b6d3e25ecb9b4b11bd8b177
Andrey Elyan <ilegra.andrey@agibank.com.br>
2021-02-21 23:23:28 -0300

    fix(css): tailwind fix

--- next.config.js
+++ next.config.js
@@ -15,3 +15,7 @@ module.exports = {
     ENV_TYPE: process.env.ENV_TYPE
   }
 };
+
+const withCSS = require('@zeit/next-css');
+
+module.exports = withCSS({});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import '../styles/globals.css';
-import 'tailwindcss/tailwind.css';
 
 import { Header } from 'components/Header';
 import Head from 'next/head';
