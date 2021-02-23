1634c64b4032d85ad702ed6f5677d880625cf9d7
Andrey Elyan <ilegra.andrey@agibank.com.br>
2021-02-23 17:21:08 -0300

    fix(container): adjust container

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -2,7 +2,7 @@ import Characters from 'containers/characters';
 
 export default function Page() {
   return (
-    <div className=" container mx-auto px-4 flex justif-center flex-col text-center">
+    <div className=" container mx-auto px-4 flex justify-center flex-col text-center">
       <Characters />
     </div>
   );
